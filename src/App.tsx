9e8828c869cf53bc51524ad158c9a8de8fd052b1
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 23:47:55 +0200

    feat: added ProductListPage to app and added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { ProductListPage } from './pages';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
