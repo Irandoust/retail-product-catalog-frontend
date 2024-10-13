ef1fe48e3c655324c90b1e09bd7e897e0d582498
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 17:19:07 +0200

    feat(docs): added required comments to the Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * Error Component
+ * Displays an error message in a Bootstrap-styled alert box.
+ * Used when there is a failure to fetch products or other errors.
+ */
 export const Error: React.FC = () => {
   return (
     <div className="alert alert-danger">
