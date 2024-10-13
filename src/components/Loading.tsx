e1ceb8284858d88fcdd232d0a8d257e20dba72c5
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 14:14:51 +0200

    feat(component): improved Loading component style using bootstrap alert

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export const Loading: React.FC = () => {
-  return <div>Loading products...</div>;
+  return (
+    <div className="alert alert-info">
+      <span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>
+      <span>&nbsp;</span>
+      Loading procuts...
+    </div>
+  );
 };
 
 export default Loading;
