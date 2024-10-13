50b3f0ff940a13e11d7c6bb9665364d5149bffd5
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 14:14:41 +0200

    feat(component): improved Error component style using bootstrap alert

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export const Error: React.FC = () => {
-  return <div>Error fetching products.</div>;
+  return (
+    <div className="alert alert-danger">
+      <span
+        className="glyphicon glyphicon-exclamation-sign"
+        aria-hidden="true"
+      ></span>
+      <span>&nbsp;</span>
+      Error fetching products.
+    </div>
+  );
 };
 
 export default Error;
