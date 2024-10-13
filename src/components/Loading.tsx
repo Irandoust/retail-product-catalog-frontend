1d2a29898e3e8639d7ab455eebd0c117027ad415
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 17:19:01 +0200

    feat(docs): added required comments to the Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * Loading Component
+ * Displays a loading message in a Bootstrap-styled alert box.
+ * Used to indicate that product data is currently being loaded.
+ */
 export const Loading: React.FC = () => {
   return (
     <div className="alert alert-info">
