25461e23ce94573b5339a1b4303fd9256d257f25
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 14:12:23 +0200

    feat(search): added debouce mechanism along with tooltip

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,39 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import '../styles/SearchBar.css';
 
 interface SearchBarProps {
   onSearch: (term: string) => void;
 }
 
+export function debounce(func: (...args: any[]) => void, delay: number) {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+}
+
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [term, setTerm] = useState('');
+  const [showTooltip, setShowTooltip] = useState(false);
 
-  const handleSearch = () => {
-    if (term.trim()) {
-      onSearch(term);
-    }
-  };
+  const debouncedSearch = useMemo(
+    () =>
+      debounce(
+        onSearch,
+        Number(process.env.REACT_APP_SEARCH_DEBOUNCE_PERIOD) || 500,
+      ),
+    [onSearch],
+  );
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSearch();
+  useEffect(() => {
+    if (term.trim().length > 0 && term.trim().length < 3) {
+      setShowTooltip(true);
+    } else {
+      setShowTooltip(false);
     }
-  };
+
+    if (term.trim().length === 0) {
+      onSearch('');
+    } else if (term.trim().length >= 3) {
+      debouncedSearch(term);
+    }
+  }, [term, debouncedSearch]);
 
   return (
-    <div className="input-group">
-      <input
-        className="form-control"
-        type="text"
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-        onKeyUp={handleKeyPress}
-        placeholder="Search for products..."
-      />
-      <span className="input-group-btn">
-        <button className="btn btn-default" onClick={handleSearch}>
-          Search
-        </button>
-      </span>
+    <div className="search-bar-container">
+      <div className=" input">
+        <input
+          className="form-control"
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          placeholder="Search for products..."
+        />
+
+        {showTooltip && (
+          <div className="tooltip">
+            Search term must be at least 3 characters
+          </div>
+        )}
+      </div>
     </div>
   );
 };
