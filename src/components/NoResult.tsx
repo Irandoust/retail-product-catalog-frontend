3a7b3473991881eb908b979e59c4f62ad360ebb5
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 17:18:53 +0200

    feat(docs): added required comments to the NoResult component

--- src/components/NoResult.tsx
+++ src/components/NoResult.tsx
@@ -4,6 +4,13 @@ interface NoResultProps {
   text: string;
 }
 
+/**
+ * NoResult Component
+ * Displays a message when no search results or data are found.
+ * The message is passed as a prop and shown inside a Bootstrap-styled alert box.
+ *
+ * @param {NoResultProps} text - The message to be displayed in the alert box
+ */
 export const NoResult: React.FC<NoResultProps> = ({ text }: NoResultProps) => {
   return (
     <div className="alert alert-info">
