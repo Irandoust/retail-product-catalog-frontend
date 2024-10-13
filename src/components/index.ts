a202e22231e1e0fb9edad86b9818fea3a57664c8
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 13:00:04 +0200

    feat: added index export for the components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export { SearchBar } from './SearchBar';
+export { ProductList } from './ProductList';
+export { ProductItem } from './ProductItem';
+export { Loading } from './Loading';
+export { Error } from './Error';
+export { Pagination } from './Pagination';
